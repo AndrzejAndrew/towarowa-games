1d82a782f66265766d2977fdcd07ad7ed143485d
andrzejandrewendrju <andrzejandrewendrju@gmail.com>
2026-01-02 09:19:30 +0000

    Refactor: Ujednolicenie połączenia z bazą danych

--- htdocs/includes/quiz_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host='localhost';$user='user';$pass='pass';$db='quiz_db';
-$conn=new mysqli($host,$user,$pass,$db);
-if($conn->connect_error){die('DB error');}
-mysqli_set_charset($conn,'utf8mb4');
-?>
